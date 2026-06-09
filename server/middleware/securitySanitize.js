0c5b8e714d1cfeef3012aa0d56757116d1da4bfa
Debasmita <dbose272@gmail.com>
2026-06-09 11:52:12 +0530

    security: sanitize user-generated input before rendering

--- server/middleware/securitySanitize.js
+++ server/middleware/securitySanitize.js
@@ -1,18 +1,22 @@
 export const sanitizeInput = (req, res, next) => {
   const cleanData = (data) => {
     if (typeof data === 'string') {
-      return data.replace(/[<>]/g, '').replace(/\$/g, '');
+      return data.replace(/[<>]/g, '');
     }
     if (typeof data === 'object' && data !== null) {
       for (const key in data) {
-        data[key] = cleanData(data[key]);
+        if (key.startsWith('$') || key.includes('.')) {
+          delete data[key];
+        } else {
+          data[key] = cleanData(data[key]);
+        }
       }
     }
     return data;
   };
 
-  req.body = cleanData(req.body);
-  req.query = cleanData(req.query);
-  req.params = cleanData(req.params);
+  if (req.body) req.body = cleanData(req.body);
+  if (req.query) req.query = cleanData(req.query);
+  if (req.params) req.params = cleanData(req.params);
   next();
 };
