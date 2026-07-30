154ab572b504e1072def5edf2d960cc55aac9cb5
Babin123456 <babin@example.com>
2026-07-30 23:57:49 +0530

    feat: add worker payouts and stripe connect express integration system

--- server/server.js
+++ server/server.js
@@ -39,10 +39,14 @@ import moderationRoutes from './routes/moderationRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
 import verificationRoutes from './routes/verificationRoutes.js';
 import disputeRoutes from './routes/disputeRoutes.js';
+import calendarRoutes from './routes/calendarRoutes.js';
+import payoutRoutes from './routes/payoutRoutes.js';
 
 import recommendationRoutes from './routes/recommendationRoutes.js';
 import reportRoutes from './routes/reportRoutes.js';
 app.use('/api/reports', reportRoutes);
+app.use('/api/calendar', calendarRoutes);
+app.use('/api/payouts', payoutRoutes);
 
 dotenv.config();
 
