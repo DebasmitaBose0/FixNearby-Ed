90f439e0b50ed7b04f19dd8da0fade074179c0ca
Babin123456 <babin@example.com>
2026-07-30 23:58:47 +0530

    feat: implement secure file attachment and image sharing in real-time messaging

--- server/server.js
+++ server/server.js
@@ -39,10 +39,12 @@ import moderationRoutes from './routes/moderationRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
 import verificationRoutes from './routes/verificationRoutes.js';
 import disputeRoutes from './routes/disputeRoutes.js';
+import attachmentRoutes from './routes/attachmentRoutes.js';
 
 import recommendationRoutes from './routes/recommendationRoutes.js';
 import reportRoutes from './routes/reportRoutes.js';
 app.use('/api/reports', reportRoutes);
+app.use('/api/attachments', attachmentRoutes);
 
 dotenv.config();
 
